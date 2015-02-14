dd5b4d0cbd9f48731bb6ca724b9624feb8b8fc35
petrap12 <petrap12@skel.ru.is>
2015-02-14 21:55:49 +0000

    caps fixed

---
+++ interpreter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Lexer.h"
+#include "Parser.h"
+using namespace std;
+
+int main(){
+    Lexer myLexer = new Lexer();
+    Parser myParser = new Parser(myLexer);
+    myParser.parse();
+
+    return 0;
+};
+
