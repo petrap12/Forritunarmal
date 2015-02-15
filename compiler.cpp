8ca5486aa73c3a39f5c583a597061ab38e562050
petrap12 <petrap12@skel.ru.is>
2015-02-15 22:14:32 +0000

    Outstream added to compiler

--- compiler.cpp
+++ compiler.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main ()
 {
-	
+	ofstream outstream;
+  	outstream.open ("compilerOutput.txt");
+  	outstream << "Writing this to a file and something else plus this.\n";
+  	outstream.close();
 	return 0;
 }
