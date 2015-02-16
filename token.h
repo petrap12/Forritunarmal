7387fc223bee8a76fe83f4cf9c28566c50e6fab5
petrap12 <petrap12@skel.ru.is>
2015-02-16 21:20:54 +0000

    token.h finished

--- token.h
+++ token.h
@@ -7,15 +7,14 @@ using namespace std;
 class Token
 {
     public:
-	    Token(string lexeme){lexeme = ""; tCode = ID;}
-
-		void setLexeme(string lex){lexeme = lex;}
-		string getLexeme(){return lexeme;}
-	private:
 		string lexeme;
 		enum TokenCode { ID, ASSIGN, SEMICOL, INT, PLUS, MINUS,
 	                    MULT, LPAREN, RPAREN, PRINT, END, ERROR};
 	    TokenCode tCode;
+
+	    Token(){lexeme = ""; tCode = ID;}
+	    Token(string lex, TokenCode code){lexeme = lex; tCode = code;}
+	
 };
 typedef Token* TokenPtr;
 
