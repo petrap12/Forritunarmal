ee1ab8cbcf59cd1a90567202c1a6af31aa987bf0
petrap12 <petrap12@skel.ru.is>
2015-02-17 15:06:37 +0000

    Token.java modified

--- Lexer.java
+++ Lexer.java
@@ -1,5 +1,12 @@
 public class Lexer
 {
+	Lexer();
+	Token nextToken();
+	void errorMessage();
+	Token thisToken;
+	char arrSymbols [];
+	enum TokenCode {allTokens = Token::ERROR};
+	
 	public static void main(String[] args) {
  		System.out.println("Hello World");
  	}	
--- Token.java
+++ Token.java
@@ -1,6 +1,14 @@
 public class Token
 {
-	public static void main(String[] args) {
- 		System.out.println("Hello World");
- 	}
+	String lexeme;
+	enum TokenCode { ID, ASSIGN, SEMICOL, INT, PLUS, MINUS,
+	                    MULT, LPAREN, RPAREN, PRINT, END, ERROR};
+	TokenCode tCode;
+
+
+	public Token(String lex, TokenCode cod)
+        {
+            this.lexeme = lex;
+            this.tCode = cod;
+        }
 }
