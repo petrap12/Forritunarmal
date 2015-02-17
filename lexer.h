e65002a035be23113f74d375d311d7370ef45cca
petrap12 <petrap12@skel.ru.is>
2015-02-17 14:00:25 +0000

    lexer.h modified

--- lexer.h
+++ lexer.h
@@ -16,7 +16,6 @@ class Lexer: public Token
 		Token thisToken;
 		string arrSymbols [];
 		enum TokenCode {allTokens = Token::ERROR};
-		//TokenCode arrTokens [];
 };
 //typedef Lexer* LexerPtr;
 
@@ -30,52 +29,22 @@ Lexer::Lexer()
 	arrSymbols[5] = ("("); //LPAREN
 	arrSymbols[6] = (")"); //RPAREN
 
-	//Don't know exactly how we implement the e's
-	/*arrSymbols[0] = ("e"); //ID as e
-	arrSymbols[1] = ("="); //ASSIGN
-	arrSymbols[2] = (";"); //SEMICOL
-	arrSymbols[3] = ("e"); //INT as e
-	arrSymbols[4] = ("+"); //PLUS
-	arrSymbols[5] = ("-"); //MINUS
-	arrSymbols[6] = ("*"); //MULT
-	arrSymbols[7] = ("("); //LPAREN
-	arrSymbols[8] = (")"); //RPAREN
-	arrSymbols[9] = ("e"); //PRINT as e
-	arrSymbols[10] = ("e"); //END as e
-	arrSymbols[11] = ("e"); //ERROR as e*/
-
-	//This should work
-	//puts all enums in the array arrTokens
-	/*int count=0;
-	for ( int fooInt = ID; fooInt != ERROR; fooInt++ )
-	{
-   		TokenCode foo = static_cast<TokenCode>(fooInt);
-   		arrTokens[count] = foo;
-   		count++;
-	}*/
-
 	//{ ID,INT,PRINT, END, ERROR};
 }
 Token Lexer::nextToken()
 {
 	bool inTokenCode=false;
-
-	//Test to understand how TokenPtr works
-	/*TokenPtr nxt = new Token("amma", INT);
-	nxt->TokenCode.ASSIGN;
-	nxt->lexeme = "moo";
-	string mama = nxt->lexeme;*/
-
 	string buff = "";
 
 	while(true)
 	{
 		getline(cin, buff); //? don't know how to get stdin in another way
 
-		for(int i=0; inTokenCode || i==6; i++)
+		for(int i=0; inTokenCode || i<7; i++)
 		{
 			if(buff == arrSymbols[i])
 			{
+				inTokenCode = true;
 				Token nxt;
 				nxt.lexeme = buff;
 				if(arrSymbols[i] == "=")
