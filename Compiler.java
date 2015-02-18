0bddd8bd31c9575bd920b3553d8f4c8dcc60a727
petrap12 <petrap12@skel.ru.is>
2015-02-18 21:38:24 +0000

    Compiler.java modified

--- Compiler.java
+++ Compiler.java
@@ -1,6 +1,10 @@
+import java.io.IOException;
+
 public class Compiler
 {
-	public static void main(String[] args) {
- 		System.out.println("Hello World");
- 	}
+	public static  void main(String[] args) throws IOException {
+		Lexer myLexer = new Lexer();
+    	Parser myParser = new Parser(myLexer);
+    	myParser.parse();
+    }
 }
